da854be576bc36cfc30534d5801f272960752a33
Rowan Hart <rowanbhart@gmail.com>
2020-03-20 19:55:18 -0400

    Move rev challenges into the correct directory

--- REV/300_railed/flag_helper.py
+++
@@ -1,4 +0,0 @@
-flag="pctf{runATR41N}"
-
-for c in flag:
-    print("{:08b} 0x{:02x} {}".format(ord(c), ord(c), c))
--- REV/300_railed/src/register.cpp
+++
@@ -1,7 +0,0 @@
-#include "register.hpp"
-
-Register::Register(std::string name, int64_t value, bool pc) {
-	this->name = name;
-	this->value = value;
-	this->pc = pc;
-}
